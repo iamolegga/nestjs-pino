b45c67352c2a7612eddf703dd1e2e3ea436ea9e7
Rhys Streefland <rhys@streefland.co.uk>
2020-04-19 16:45:08 +0100

    feat: create injection token in exported function so tests can get the injection token to override the logger

--- src/InjectPinoLogger.ts
+++ src/InjectPinoLogger.ts
@@ -7,12 +7,12 @@ const decoratedLoggers = new Set<string>();
 
 export function InjectPinoLogger(context = "") {
   decoratedLoggers.add(context);
-  return Inject(`${decoratedTokenPrefix}${context}`);
+  return Inject(getLoggerToken(context));
 }
 
 function createDecoratedLoggerProvider(context: string): Provider<PinoLogger> {
   return {
-    provide: `${decoratedTokenPrefix}${context}`,
+    provide: getLoggerToken(context),
     useFactory: (logger: PinoLogger) => {
       logger.setContext(context);
       return logger;
@@ -25,4 +25,8 @@ export function createProvidersForDecorated(): Array<Provider<PinoLogger>> {
   return [...decoratedLoggers.values()].map(context =>
     createDecoratedLoggerProvider(context)
   );
+}
+
+export function getLoggerToken(context: string): string {
+  return `${decoratedTokenPrefix}${context}`;
 }
