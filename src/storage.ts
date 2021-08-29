f0687e47ebc85762a622630cad68c3409fb58043
iamolegga <iamolegga@gmail.com>
2021-08-29 23:37:25 +0300

    assign

--- src/storage.ts
+++ src/storage.ts
@@ -1,4 +1,8 @@
 import { AsyncLocalStorage } from 'async_hooks';
 import { Logger } from 'pino';
 
-export const storage = new AsyncLocalStorage<Logger>();
+export class Store {
+  constructor(public logger: Logger) {}
+}
+
+export const storage = new AsyncLocalStorage<Store>();
