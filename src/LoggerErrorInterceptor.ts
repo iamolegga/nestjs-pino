0a3031c45d91830720355e87ea49cdc20a50863a
Antanas Šepikas <antanas.sepikas@telesoftas.com>
2021-12-16 08:03:00 +0200

    feat: add logger error interceptor

---
+++ src/LoggerErrorInterceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class LoggerErrorInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next
+      .handle()
+      .pipe(
+        catchError((error) =>
+          throwError((context.switchToHttp().getResponse().err = error)),
+        ),
+      );
+  }
+}
