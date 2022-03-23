246847e2bc77429a11a3ac38da6170c9ccc60c2c
iamolegga <iamolegga@gmail.com>
2022-03-23 22:58:17 +0400

    help ukraine

---
+++ postinstall.js
@@ -0,0 +1,18 @@
+console.log(`
+
+
+
+
+\t\tThanks for using nestjs-pino!
+
+\t\tAs you may know there is a war against Ukrain:
+\t\tbombed cities (including schools, hospitals, etc), hundreds of killed children, millions of refugees.
+\t\tSo if you enjoy this library you can thank the author by donating to one of this funds:
+\t\t• https://war.ukraine.ua/donate/ – Armed Forces of Ukraine
+\t\t• https://www.comebackalive.in.ua/ – Ukrainian volunteers
+
+\t\tThanks for your support!
+
+`);
+
+new Promise(r => setTimeout(r, 5000));
